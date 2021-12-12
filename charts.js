0fc1ae4cd231907a0e36fed5186075f18d38f849
JFo <88628933+JFoArlas@users.noreply.github.com>
2021-12-11 18:42:50 -0700

    Deliverable 1

--- charts.js
+++ charts.js
@@ -62,7 +62,7 @@ function buildCharts(sample) {
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var samplesArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-    var result = resultArray[0];
+    var result = samplesArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = result.otu_ids;
